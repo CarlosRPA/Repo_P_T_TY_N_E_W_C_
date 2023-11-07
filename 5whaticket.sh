2b067f333443322da0817186b8e43c396fb518d6
Carlos Frazão <149631148+CarlosRPA@users.noreply.github.com>
2023-11-06 23:00:56 -0300

    Update 5whaticket.sh

--- 5whaticket.sh
+++ 5whaticket.sh
@@ -12,13 +12,13 @@ cd
 cd
 
 # Baixa o instalador do Whaticket
-sudo apt install -y git && git clone https://github.com/Rickinet/chat_zap.git
+sudo apt install -y git && git clone https://github.com/CarlosRPA/install_WT_RPA.git
 
 # Dá permissões de execução ao script de instalação
-sudo chmod -R 777 chat_zap
+sudo chmod -R 777 install_WT_RPA
 
 # Entrar no diretorio
-cd chat_zap
+cd install_WT_RPA
 
 # Executa o script de instalação do Chatwoot
 sudo ./install_primaria
@@ -27,19 +27,17 @@ sudo ./install_primaria
 
 cd
 
-cd
-
 clear
 
-cd ~/instalador_T_N_E
+cd /home/ubuntu/install_P_T_TY_N_E_W_C_/Servidor_TY_E_A_C_W_N_N_
 
-# Retorna para o instalador.sh
+# Retorna para o servidor.sh
 # Exibe uma mensagem de confirmação
 read -p "Deseja voltar para o MENU PRINCIPAL? (Y/N): " choice
 
 # Verifica a escolha do usuário
 if [ "$choice" == "Y" ] || [ "$choice" == "y" ]; then
-  sudo chmod +x instalador.sh && ./instalador.sh
+  sudo chmod +x servidor.sh && ./servidor.sh
   echo "Comando executado."
 elif [ "$choice" == "N" ] || [ "$choice" == "n" ]; then
   echo "Comando não executado. Continuando..."
