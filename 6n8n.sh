6afc08a01f8ced6c6d3e81e4b0f1597cc2d24e15
Carlos Frazão <149631148+CarlosRPA@users.noreply.github.com>
2024-07-13 14:10:02 -0300

    Update 6n8n.sh

--- 6n8n.sh
+++ 6n8n.sh
@@ -1,100 +1,163 @@
 #!/bin/bash
 
-# Função para exibir mensagens de status
-status() {
-    echo -e "\n\e[1m$1\e[0m\n"
-}
+#########################################################
 
-# Atualizar pacotes do Ubuntu
-status "Atualizando pacotes do Ubuntu..."
-sudo apt update
-sudo apt upgrade -y
-
-# Solicitação do subdomínio
-read -p "Por favor, insira o subdomínio para instalar o n8n: " subdominio
-
-# Solicitação do email para o certificado SSL
-read -p "Por favor, insira seu endereço de e-mail para instalar o certificado SSL: " email
-
-# Instalação do Node.js 18
-status "Instalando Node.js 20..."
-curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
-sudo apt-get install -y nodejs
-
-# Instalação do npm patch-package
-status "Instalando npm patch-package..."
-sudo npm install -g patch-package
-
-# Instalação do n8n
-status "Instalando n8n..."
-sudo npm install n8n@latest -g
-
-# Criação do arquivo ecosystem.config.js
-status "Configurando n8n..."
-sudo tee /usr/lib/node_modules/n8n/ecosystem.config.js <<EOF
-module.exports = {
-    apps : [{
-        name   : "n8n",
-        script : "n8n",
-        cwd    : "/usr/lib/node_modules/n8n",
-        env: {
-            N8N_PROTOCOL: "https",
-            WEBHOOK_TUNNEL_URL: "https://$subdominio/",
-            N8N_HOST: "$subdominio"
-        }
-    }]
-}
-EOF
-
-# Instalação do PM2
-status "Instalando PM2..."
-sudo npm install pm2@latest -g
-
-# Instalação do Nginx
-status "Instalando Nginx..."
-sudo apt-get install -y nginx
-
-# Configuração do Nginx
-status "Configurando Nginx..."
-sudo tee /etc/nginx/sites-available/n8n <<EOF
-server {
-    listen 80;
-    server_name $subdominio;
-
-    location / {
-        proxy_pass http://localhost:5678;
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection 'upgrade';
-        proxy_set_header Host \$host;
-        proxy_cache_bypass \$http_upgrade;
-    }
-}
-EOF
-
-sudo ln -s /etc/nginx/sites-available/n8n /etc/nginx/sites-enabled/
-
-# Reinicie o Nginx para aplicar as alterações
-status "Reiniciando Nginx..."
-sudo systemctl restart nginx
-
-# Instalação do Certbot e plugins do Nginx
-status "Instalando Certbot e plugins do Nginx..."
-sudo apt-get install -y certbot python3-certbot-nginx
-
-
-
-# Solicitação e configuração do certificado SSL com o Certbot
-status "Configurando certificado SSL..."
-sudo certbot --nginx -d $subdominio --email $email
-
-# Iniciar o n8n com PM2
-status "Iniciando n8n com PM2..."
-pm2 start /usr/lib/node_modules/n8n/ecosystem.config.js
-
-# Mensagem final
 clear
-echo -e "\n\e[32mInstalação concluída com sucesso!\e[0m"
-echo "-------------------------------------"
-echo "Você pode acessar o n8n através da seguinte URL:"
-echo -e "\n\e[34mhttps://$subdominio\e[0m\n"
+
+echo -e "\e[32m
+
+        ██████╗ ██╗   ██╗    ██████╗ ██████╗  █████╗ 
+        ██╔══██╗╚██╗ ██╔╝    ██╔══██╗██╔══██╗██╔══██╗
+        ██████╔╝ ╚████╔╝     ██████╔╝██████╔╝███████║
+        ██╔══██╗  ╚██╔╝      ██╔══██╗██╔═══╝ ██╔══██║
+        ██████╔╝   ██║       ██║  ██║██║     ██║  ██║
+        ╚═════╝    ╚═╝       ╚═╝  ╚═╝╚═╝     ╚═╝  ╚═╝\e[0m"
+echo -e "\e[32m                          BY RPA                                      \e[0m"
+echo -e "\e[32m                  AUTOR ==> CARLOS FRAZÃO <==                           \e[0m"
+echo -e "\e[32m\e[0m"
+
+#########################################################
+
+echo ""
+echo -e "\n\033[31m              ╔════════════════════════════════════════════════════════╗\033[0m"
+echo -e "\033[31m              ║                                                        ║\033[0m"
+echo -e "\033[31m              ║  \033[34m     Preencha as informações solicitadas abaixo       \033[31m║\033[0m"
+echo -e "\033[31m              ║                                                        ║\033[0m"
+echo -e "\033[31m              ╚════════════════════════════════════════════════════════╝\033[0m\n"
+echo ""
+echo ""
+echo ""
+echo -e "\e[93mPasso \e[33m1/4\e[0m"
+read -p "Digite a versão que deseja instalar do N8N (ex: latest | ex: 1.15.1): " versionnn
+echo ""
+echo -e "\e[93mPasso \e[33m2/4\e[0m"
+read -p "Digite o dominio para acessar o N8N (ex: n8n.dominio.com): " dominionn
+echo ""
+echo -e "\e[93mPasso \e[33m3/4\e[0m"
+read -p "Digite a porta para o N8N (padrão: 5678): " portann
+echo ""
+echo -e "\e[93mPasso \e[33m4/4\e[0m"
+read -p "Digite seu email para o proxy reverso (ex: contato@dominio.com): " emailnn
+echo ""
+
+#########################################################
+
+# VERIFICAÇÃO DE DADOS
+
+clear
+
+echo -e "\n\033[31m              ╔════════════════════════════════════════════════════════╗\033[0m"
+echo -e "\033[31m              ║                                                        ║\033[0m"
+echo -e "\033[31m              ║  \033[34m   Verifique se os dados abaixos estão ccorretos      \033[31m║\033[0m"
+echo -e "\033[31m              ║                                                        ║\033[0m"
+echo -e "\033[31m              ╚════════════════════════════════════════════════════════╝\033[0m\n"
+
+echo ""
+echo -e "Link do N8N: \e[33m$dominionn\e[0m"
+echo -e "Porta: \e[33m$portann\e[0m"
+echo -e "Email: \e[33m$emailnn\e[0m"
+echo ""
+echo ""
+read -p "As informações estão certas? (y/n): " confirma2
+if [ "$confirma2" == "y" ]; then
+
+#########################################################
+
+# EXECUTANDO O N8N
+
+cd
+
+cd
+
+clear
+
+sudo npm install -g npm@latest --force
+
+sudo npm install -g n8n@$versionnn --force
+
+sudo npm install -g pm2@latest --force
+
+sudo apt-get update
+
+sudo apt-get upgrade -y
+
+#########################################################
+
+# FAZENDO PROXY REVERSO
+
+cat > n8n << EOL
+server {
+  server_name $dominionn;
+  
+  underscores_in_headers on;
+
+  location / {
+
+   proxy_pass http://127.0.0.1:$portann;
+   proxy_pass_header Authorization;
+   proxy_set_header Upgrade \$http_upgrade;
+   proxy_set_header Connection "upgrade";
+   proxy_set_header Host \$host;
+   proxy_set_header X-Forwarded-Proto \$scheme;
+   proxy_set_header X-Forwarded-Ssl on; # Optional
+   proxy_set_header X-Real-IP \$remote_addr;
+   proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+   proxy_http_version 1.1;
+   proxy_set_header Connection "";
+   proxy_buffering off;
+   client_max_body_size 0;
+   proxy_read_timeout 36000s;
+   proxy_redirect off;
+  }
+  add_header Strict-Transport-Security "max-age=31536000; includeSubDomains" always;
+  ssl_protocols TLSv1.2 TLSv1.3;
+}
+EOL
+
+sudo mv n8n /etc/nginx/sites-available/n8n
+
+sudo ln -s /etc/nginx/sites-available/n8n /etc/nginx/sites-enabled
+
+sudo certbot --nginx --email $emailnn --redirect --agree-tos -d $dominionn
+
+sudo systemctl reload nginx
+
+#########################################################
+
+elif [ "$confirma2" == "n" ]; then
+    echo "Encerrando a instalação, por favor, inicie a instalação novamente."
+    exit 0
+else
+    echo "Resposta inválida. Digite 'y' para confirmar ou 'n' para encerrar a instalação."
+    exit 1
+fi
+
+#######################################################
+
+# INICIANDO PM2
+
+pm2 start n8n --cron-restart="01 0 * * *" -- start
+
+sudo pm2 startup ubuntu -u root && sudo pm2 startup ubuntu -u root --hp /root && sudo pm2 save
+
+#######################################################
+
+cd
+
+clear
+
+cd /home/ubuntu/install_P_T_TY_N_E_W_C_/Servidor_TY_E_A_C_W_N_N_
+
+# Retorna para o servidor.sh
+# Exibe uma mensagem de confirmação
+read -p "Deseja voltar para o MENU PRINCIPAL? (Y/N): " choice
+
+# Verifica a escolha do usuário
+if [ "$choice" == "Y" ] || [ "$choice" == "y" ]; then
+  sudo chmod +x servidor.sh && ./servidor.sh
+  echo "Comando executado."
+elif [ "$choice" == "N" ] || [ "$choice" == "n" ]; then
+  echo "Comando não executado. Continuando..."
+else
+  echo "Escolha inválida. Saindo."
+fi
