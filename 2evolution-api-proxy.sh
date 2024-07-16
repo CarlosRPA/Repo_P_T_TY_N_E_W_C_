1a898465e99180d9e8263f74ab8fe86b16c42b5e
Carlos Frazão <149631148+CarlosRPA@users.noreply.github.com>
2024-07-16 19:44:27 -0300

    Update 2evolution-api-proxy.sh

--- 2evolution-api-proxy.sh
+++ 2evolution-api-proxy.sh
@@ -150,7 +150,7 @@ cd evolution-api
 
 apt install npm -f
 
-git pull
+#git pull
 
 #git branch -a
 
@@ -474,6 +474,82 @@ location ~ /\.ht {
 
 cd
 
+cd /etc/nginx/
+
+# Remova o arquivo nginx.conf se ele existir
+sudo rm -f nginx.conf
+
+# Crie o arquivo nginx.conf com o conteúdo desejado automaticamente
+sudo tee nginx.conf > /dev/null <<EOL
+user www-data;
+worker_processes auto;
+pid /run/nginx.pid;
+include /etc/nginx/modules-enabled/*.conf;
+
+events {
+        worker_connections 768;
+        # multi_accept on;
+}
+
+http {
+
+        ##
+        # Basic Settings
+        ##
+
+        sendfile on;
+        tcp_nopush on;
+        tcp_nodelay on;
+        keepalive_timeout 65;
+        types_hash_max_size 2048;
+        # server_tokens off;
+
+        server_names_hash_bucket_size 256;
+        # server_name_in_redirect off;
+
+        include /etc/nginx/mime.types;
+        default_type application/octet-stream;
+
+        ##
+        # SSL Settings
+        ##
+
+        ssl_protocols TLSv1 TLSv1.1 TLSv1.2 TLSv1.3; # Dropping SSLv3, ref: POODLE
+        ssl_prefer_server_ciphers on;
+
+        ##
+        # Logging Settings
+        ##
+
+        access_log /var/log/nginx/access.log;
+        error_log /var/log/nginx/error.log;
+
+        ##
+        # Gzip Settings
+        ##
+
+        gzip on;
+
+        # gzip_vary on;
+        # gzip_proxied any;
+        # gzip_comp_level 6;
+        # gzip_buffers 16 8k;
+        # gzip_http_version 1.1;
+        # gzip_types text/plain text/css application/json application/javascript text/xml application/xml application/xml+rss text/javascript;
+
+        ##
+        # Virtual Host Configs
+        ##
+
+        include /etc/nginx/conf.d/*.conf;
+        include /etc/nginx/sites-enabled/*;
+}
+EOL
+
+#######################################################
+
+cd
+
 systemctl reload nginx
 
 chown www-data:www-data /usr/share/nginx/html -R
@@ -516,13 +592,24 @@ systemctl reload nginx
 
 #########################################################
 
+# Atualiza os repositórios
 apt-get update
 
+# Realiza a atualização do sistema
 apt-get upgrade -y
 
-echo "proxy reverso da Evolution e do typebot"
+# Reinicia o serviço Nginx
+sudo systemctl restart nginx
 
-snap install --classic certbot
+#######################################################
+
+cd
+
+# Instala o Certbot e o plugin para o Nginx
+sudo apt-get install certbot python3-certbot-nginx -y
+
+# Instala o Certbot via Snap
+sudo snap install --classic certbot
 
 certbot --nginx --email $emaill --redirect --agree-tos -d $dominio 
 
